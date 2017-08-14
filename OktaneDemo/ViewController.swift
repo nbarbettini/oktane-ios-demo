a646083de6ab1a6cac1cde92153ec21ca8bed9ad
nbarbettini <nathanaelb@gmail.com>
2017-08-14 14:13:39 -0700

    Initial commit

---
+++ OktaneDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OktaneDemo
+//
+//  Created by Nathanael Barbettini on 8/14/17.
+//  Copyright © 2017 Okta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
