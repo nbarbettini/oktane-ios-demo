e367ec949a245bbbe52c11f03beb44fcf5cbd08a
nbarbettini <nathanaelb@gmail.com>
2017-08-14 18:01:39 -0700

    Simplify loginWasPressed

--- OktaneDemo/MainViewController.swift
+++ OktaneDemo/MainViewController.swift
@@ -27,29 +27,24 @@ class MainViewController: UIViewController {
             .start(self) {
                 response, error in
                 
-                if error != nil { print(error!) }
-                
-                if response != nil {
+                OktaAuth.userinfo() {
+                    response, error in
                     
-                    // Get the user info
-                    OktaAuth.userinfo() {
-                        response, error in
-                        
-                        if error != nil { print(error!) }
-                        
-                        let profileView = self.storyboard?.instantiateViewController(
-                            withIdentifier: "profile") as! ProfileViewController
-                        
-                        profileView.email = String(describing: response!["preferred_username"]!)
-                        profileView.givenName = String(describing: response!["given_name"]!)
-                        
-                        DispatchQueue.main.async(execute: {
-                            self.present(profileView, animated: true, completion: nil)
-                        })
-                    }
+                    self.showProfileWith(data: response!)
                 }
         }
+    }
+    
+    func showProfileWith(data: [String : Any]) {
+        let profileView = self.storyboard?.instantiateViewController(
+            withIdentifier: "profile") as! ProfileViewController
         
+        profileView.email = String(describing: data["preferred_username"]!)
+        profileView.givenName = String(describing: data["given_name"]!)
+        
+        DispatchQueue.main.async(execute: {
+            self.present(profileView, animated: true, completion: nil)
+        })
     }
 
 }
